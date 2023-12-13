09c94264592eec23d455c047acc664fca5158ca7
Jonathan <jdguayllas@utpl.edu.ec>
2023-12-13 16:27:15 -0500

    arreglo + acumulador + suma

--- Ejemplos/src/paquete1/Ejemplo041.java
+++ Ejemplos/src/paquete1/Ejemplo041.java
@@ -15,10 +15,11 @@ public class Ejemplo041 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // variables acumuladoras
         String cadena = "";
+        int suma = 0;
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         // int[] arreglo1 = new int[5];
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
@@ -28,13 +29,21 @@ public class Ejemplo041 {
         // arreglo me da el total de elementos asociados al arreglo
         //              i < 5
         for (int i = 0; i < arreglo1.length; i++) {
+            // la variable suma se suma con el elemento que tenga el arreglo
+            suma = suma + arreglo1[i];
+            // la cadena acumula la información
             cadena = String.format("%sSubíndice o Índice %d, con valor "
                     + "%d\n",
                     cadena,
-                    i, 
+                    i,
                     arreglo1[i]);
         }
-        System.out.println(cadena);
+        // termino de darle la información a la cadena
+        cadena = String.format("%sLa suma total es: %d\n ",
+                cadena,
+                suma);
+        // presento todo en mayúsculas usando (toUpperCase())
+        System.out.println(cadena.toUpperCase());
     }
 
 }
